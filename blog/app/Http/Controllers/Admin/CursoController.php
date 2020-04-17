4b52a060c1d3622dd2e7c5448800fff55ce0d1f7
joaokage1 <j_vgomes@hotmail.com>
2020-04-17 08:58:07 -0300

    start creating CRUD of cursos

--- blog/app/Http/Controllers/Admin/CursoController.php
+++ blog/app/Http/Controllers/Admin/CursoController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Curso;
 
 class CursoController extends Controller
 {
-    //
+    public function index(){
+        $registros = Curso::all();
+        return view('admin.cursos.index', compact('registros'));
+    }
 }
--- blog/resources/views/layout/_includes/footer.blade.php
+++ blog/resources/views/layout/_includes/footer.blade.php
@@ -1,2 +1,12 @@
-</body>
-</html>
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        <script type="text/javascript">
+          $(document).read(function(){
+            Materialize.updateTextFields();
+          });
+          $(document).ready(function(){
+            $('.sidenav').sidenav();
+          });
+        </script>
+    </body>
+  </html>
